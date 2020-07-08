508f02e093bbec54c44fe09b391e9d5220209cfb
aurélien <achatelain62000@gmail.com>
2020-07-08 20:28:11 +0200

    Modif pdf

--- src/components/curriculums/PdfCurriculum.js
+++ src/components/curriculums/PdfCurriculum.js
@@ -1,15 +1,22 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux";
 import {
   PDFViewer,
   Page,
   Text,
   View,
   Document,
+  Image,
   StyleSheet,
 } from "@react-pdf/renderer";
+import { Spin } from "antd";
 
-export default class PdfCurriculum extends Component {
+class PdfCurriculum extends Component {
   render() {
+    const { cv } = this.props;
+
     const styles = StyleSheet.create({
       page: {
         flexDirection: "row",
@@ -20,20 +27,56 @@ export default class PdfCurriculum extends Component {
         padding: 10,
         flexGrow: 1,
       },
+      viewImage: {
+        width: "100px",
+        height: "100px",
+      },
+      image: {
+        objectFit: "cover",
+      },
     });
-    return (
-      <PDFViewer width="1024px" height="768px">
-        <Document>
-          <Page size="A4" style={styles.page}>
-            <View style={styles.section}>
-              <Text>Section #1</Text>
-            </View>
-            <View style={styles.section}>
-              <Text>Section #2</Text>
-            </View>
-          </Page>
-        </Document>
-      </PDFViewer>
-    );
+    if (cv) {
+      return (
+        <PDFViewer width="1024px" height="768px">
+          <Document>
+            <Page size="A4" style={styles.page}>
+              <View style={styles.section}>
+                <View style={styles.viewImage}>
+                  <Image source={cv.image} style={styles.image} />
+                </View>
+                <Text>{cv.titre}</Text>
+              </View>
+              <View style={styles.section}>
+                <Text>Section #2</Text>
+              </View>
+            </Page>
+          </Document>
+        </PDFViewer>
+      );
+    } else {
+      return (
+        <div className="center">
+          <Spin
+            size="large"
+            tip="Chargement du cv..."
+            style={{ textAlign: "center", marginTop: 200 }}
+          />
+        </div>
+      );
+    }
   }
 }
+
+const mapStateToProps = (state, ownProps) => {
+  const id = ownProps.match.params.id;
+  const curriculums = state.firestore.data.curriculums;
+  const curriculum = curriculums ? curriculums[id] : null;
+  return {
+    cv: curriculum,
+  };
+};
+
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([{ collection: "curriculums" }])
+)(PdfCurriculum);
