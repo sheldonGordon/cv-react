56120c110ceafc7f05a2daea1e2430bbba14dd70
aurélien <achatelain62000@gmail.com>
2020-08-03 21:05:12 +0200

    Partie gauche cv pdf

--- src/components/curriculums/PdfCurriculum.js
+++ src/components/curriculums/PdfCurriculum.js
@@ -13,6 +13,8 @@ import {
 } from "@react-pdf/renderer";
 import { Spin } from "antd";
 
+import { GiTeacher } from "react-icons/gi";
+
 class PdfCurriculum extends Component {
   render() {
     const { cv } = this.props;
@@ -23,13 +25,39 @@ class PdfCurriculum extends Component {
         backgroundColor: "#E4E4E4",
       },
       section: {
-        margin: 10,
-        padding: 10,
+        margin: 5,
+        padding: 5,
         flexGrow: 1,
+      },
+      sectionLeft: {
+        maxWidth: "35%",
+        backgroundColor: "blue",
+      },
+      sectionRight: {
+        maxWidth: "65%",
+        backgroundColor: "red",
+      },
+      titel1: {
+        fontSize: "22px",
+        marginBottom: "12px",
+        textAlign: "center",
+      },
+      titel2: {
+        fontSize: "16px",
+        marginBottom: "10px",
+      },
+      titel3: {
+        fontSize: "14px",
+        marginBottom: "6px",
+      },
+      titel4: {
+        fontSize: "12px",
+        marginBottom: "5px",
       },
       viewImage: {
         width: "100px",
         height: "100px",
+        marginBottom: "15px",
       },
       image: {
         objectFit: "cover",
@@ -40,14 +68,54 @@ class PdfCurriculum extends Component {
         <PDFViewer width="1024px" height="768px">
           <Document>
             <Page size="A4" style={styles.page}>
-              <View style={styles.section}>
+              <View style={[styles.section, styles.sectionLeft]}>
                 <View style={styles.viewImage}>
                   <Image source={cv.image} style={styles.image} />
                 </View>
-                <Text>{cv.titre}</Text>
+                <Text style={styles.titel2}>Données personnelles</Text>
+                <Text style={styles.titel3}>Nom / Prénom :</Text>
+                <Text style={styles.titel4}>
+                  {cv.nom ? cv.nom.toUpperCase() : null}{" "}
+                  {cv.prenom
+                    ? cv.prenom[0].toUpperCase() +
+                      cv.prenom.slice(1).toLowerCase()
+                    : null}
+                </Text>
+                <Text style={styles.titel3}>Adresse : </Text>
+                <Text style={styles.titel4}>{cv.adresse}</Text>
+                <Text style={styles.titel3}>E-mail : </Text>
+                <Text style={styles.titel4}>{cv.email}</Text>
+                <Text style={styles.titel3}>Téléphone : </Text>
+                <Text style={styles.titel4}>{cv.telephone}</Text>
+                <Text style={styles.titel3}>Date de naissance : </Text>
+                <Text style={styles.titel4}>{cv.naissance}</Text>
+                <Text style={styles.titel3}>Langues : </Text>
+                <Text style={styles.titel4}>
+                  {cv.langues.map((langue) => {
+                    return (
+                      <Text key={langue.id}>
+                        {langue.libelle}
+                        {": "}
+                        {langue.niveau}
+                      </Text>
+                    );
+                  })}{" "}
+                </Text>
+                <Text style={styles.titel3}>Loisirs : </Text>
+                <Text style={styles.titel4}>
+                  {cv.loisirs.map((loisir) => {
+                    return cv.loisirs[cv.loisirs.length - 1] === loisir
+                      ? `${loisir.libelle}.`
+                      : `${loisir.libelle}, `;
+                  })}
+                </Text>
               </View>
-              <View style={styles.section}>
-                <Text>Section #2</Text>
+              <View style={[styles.section, styles.sectionRight]}>
+                <Text style={styles.titel1}>{cv.titre}</Text>
+                <Text style={styles.titel4}>{cv.description}</Text>
+                <Text style={styles.titel2}>Formations</Text>
+                <Text style={styles.titel2}>Expériences</Text>
+                <Text style={styles.titel2}>Compétences</Text>
               </View>
             </Page>
           </Document>
